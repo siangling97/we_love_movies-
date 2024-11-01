b791eb1fb861439eee1213800820aae3454dd561
Siang Ling <siangling@Siangs-MacBook-Pro.local>
2024-10-31 22:35:03 -0500

    Update server to use PORT environment variable

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const app = require("./app");
 const knex = require("./db/connection");
 
-const PORT = process.env.PORT || 5005; // Use the PORT environment variable provided by Render
+const PORT = process.env.PORT || 5005;
+console.log("Environment PORT:", process.env.PORT); // Add this line to debug
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
